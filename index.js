4e3057d94869b06c0099913207a4f21eba31e54e
JoeydaGOAT-coder <joey@kilians.us>
2025-10-29 18:03:39 -0500

    Initialize index.js with basic DOM elements

---
+++ index.js
@@ -0,0 +1,25 @@
+const expressionOut = document.getElementById("expresion");
+const answerOut = document.getElementById("output");
+const enterBtn = document.getElementById("enter-btn");
+
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const zero = document.getElementById("0");
+
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
+let expression = [];
+let answer;
+
+function addDigit () {
+  
