334a88fa859afc27be877445c2bec987179eb79e
JoeydaGOAT-coder <joey@kilians.us>
2025-10-29 19:27:37 -0500

    Add number and operation objects to index.js

--- index.js
+++ index.js
@@ -18,8 +18,63 @@ const subtract = document.getElementById("subtract");
 const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 
+let One = {
+  type: "number",
+  value: 1
+};
+let Two = {
+  type: "number",
+  value: 2
+};
+let Three = {
+  type: "number",
+  value: 3
+};
+let Four = {
+  type: "number",
+  value: 4
+};
+let Five = {
+  type: "number",
+  value: 5
+};
+let Six = {
+  type: "number",
+  value: 6
+};
+let Seven = {
+  type: "number",
+  value: 7
+};
+let Eight = {
+  type: "number",
+  value: 8
+};
+let Nine = {
+  type: "number",
+  value: 9
+};
+let Zero = {
+  type: "number",
+  value: 0
+};
+
+let Plus = {
+  type: "add"
+};
+let Minus = {
+  type: "subtract"
+};
+let Multiply = {
+  type: "multiply"
+};
+let Divide = {
+  type: "divide"
+};
+
 let expression = [];
 let answer;
 
-function addDigit () {
-  
+function addDigit (digit) {
+  if (expression.length() > 1) {
+    if (expression
